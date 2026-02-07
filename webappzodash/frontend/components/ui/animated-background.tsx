a4798aeb92ab848bad5f14befff69aeb4c545d25
thebakshicompany-hue <thebakshicompany@gmail.com>
2026-02-07 10:27:08 +0530

    feat: add more background animations (particles, orbs, waves)

--- webappzodash/frontend/components/ui/animated-background.tsx
+++ webappzodash/frontend/components/ui/animated-background.tsx
@@ -5,9 +5,27 @@ export function AnimatedBackground() {
     return (
         <div className="fixed inset-0 -z-50 overflow-hidden pointer-events-none">
             <div className="animated-bg absolute inset-0 w-full h-full">
+                {/* Main rotating circles */}
                 <div className="animated-circle circle-1"></div>
                 <div className="animated-circle circle-2"></div>
                 <div className="animated-circle circle-3"></div>
+
+                {/* Floating particles */}
+                <div className="particle particle-1"></div>
+                <div className="particle particle-2"></div>
+                <div className="particle particle-3"></div>
+                <div className="particle particle-4"></div>
+                <div className="particle particle-5"></div>
+                <div className="particle particle-6"></div>
+
+                {/* Glowing orbs */}
+                <div className="orb orb-1"></div>
+                <div className="orb orb-2"></div>
+                <div className="orb orb-3"></div>
+
+                {/* Wave lines */}
+                <div className="wave wave-1"></div>
+                <div className="wave wave-2"></div>
             </div>
         </div>
     );
